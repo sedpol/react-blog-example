1d3fb3b25e62c7d53babf98b7bcfd0395adcdfe1
sedpol <sedpol@gmail.com>
2018-10-20 17:21:13 +0100

    reactjs blog example

---
+++ src/containers/Blog/Blog.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import { Route, NavLink } from 'react-router-dom';
+
+import './Blog.css';
+import Posts from '../Blog/Posts/Posts';
+import NewPost from './NewPost/NewPost';
+
+class Blog extends Component {
+
+    render() {
+
+        return (
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><NavLink to="/"
+                                exact
+                                activeClassName="my-active" // default active
+                                activeStyle = {{
+                                    color:'#fa923f',
+                                    textDecoration: 'underline'
+                                }}
+                            >Home </NavLink> </li>
+                            <li><NavLink to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }} >New Post </NavLink></li>
+                        </ul>
+                    </nav>
+                </header>
+                {/*<Route
+                    path='/'
+                    exact
+                    render={() => <Posts/> }>
+                </Route>*/}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" exact component={NewPost} />
+            </div>
+        );
+    }
+}
+
+export default Blog;
